f834d940b839fd661975c3504fd4341479e9c38b
Matheus Carlos <78445685+Vuttley@users.noreply.github.com>
2023-06-21 21:55:58 -0300

    Update server.py

--- server.py
+++ server.py
@@ -6,13 +6,30 @@ class DBList(rpyc.Service):
   value = []
 
   def exposed_append(self, data):
-    self.value = self.value + [data]
+    self.value = self.value + [int(data)]
     return self.value
 
   def exposed_value(self):
     return self.value
 
+  def exposed_delete(self, data):
+    self.value.pop(int(data))
+    return self.value
+
+  def exposed_ordenate(self):
+    self.value.sort()
+    return self.value
+
+  def exposed_delete_element(self, data):
+    self.value.remove(int(data))
+    return self.value
+
+  def exposed_count(self, data):
+    return self.value.count(int(data))
+
+  def exposed_search(self, data):
+    return self.value.index(int(data))
+
 if __name__ == "__main__":
   server = ThreadedServer(DBList(), port = PORT)
   server.start()
-
